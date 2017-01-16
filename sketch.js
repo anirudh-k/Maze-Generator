06a4b0ef58bea5ea074e13e170bc17ce9a2a81d0
anirudhkaushik <kaushik.an@husky.neu.edu>
2017-01-16 17:25:42 -0500

    initial starter files

---
+++ sketch.js
@@ -0,0 +1,10 @@
+// required for p5.js project
+function setup() {
+  	createCanvas(800, 800);
+}
+
+// required for p5.js project
+// initiates the branch recursion for drawing the tree
+function draw() {
+  	background(51);
+}
